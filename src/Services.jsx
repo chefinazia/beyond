3ba94887079f6e4727fdcb76573e97a9d995fb33
chefinazia <rachitarora1993@gmail.com>
2024-09-21 18:51:26 +0530

    adjust width and height of screen

--- src/Services.jsx
+++ src/Services.jsx
@@ -3,17 +3,17 @@ import React from "react";
 const Services = () => {
   return (
     <>
-      <div className="relative justify-items-start ">
+      <div className="relative justify-items-start">
         <img
           src="../images/39580282-3cb4-11e8-bbb9-0242ac11000f-jpg-hero_image.jpeg"
           alt="img"
-        />
+        className="h-[600px] w-screen"/>
         <div className="w-full absolute top-0 left-2 text-center mt-10">
           <h2 className="text-4xl font-serif font-bold text-gray-50 text-left left-2">
             Quality Health Care
           </h2>
           <h2 className="text-4xl font-serif py-4 text-gray-50 text-left">
-            Book an appointment with expert doctors
+            Book an appointment with our expert doctors
           </h2>
           <a className="w-[150px] absolute left-8 px-2.5 py-2 mt-2 bg-green-800 text-left align-left text-white content-start font-semibold rounded">
             <svg
